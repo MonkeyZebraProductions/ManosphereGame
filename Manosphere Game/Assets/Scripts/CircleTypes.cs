a0a79d8c3f1ee8a36d332360da6650f8b28771eb
MonkeyZebraProductions <conormulgrewmzp@gmail.com>
2026-03-23 16:10:54 +0100

    Added Text Localisation

---
+++ Manosphere Game/Assets/Scripts/CircleTypes.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Localization;
+
+[System.Serializable]
+public struct CircleStruct
+{
+    public LocalizedStringTable LocalizedStringTable;
+    public Sprite CircleSprite;
+}
+public enum CircleEnum
+{
+    Gamer,
+    Sports,
+    Film,
+    Music,
+    Enemy
+}
+public class CircleTypes : MonoBehaviour
+{
+    public CircleStruct GamerStruct;
+    public CircleStruct SportsStruct;
+    public CircleStruct FilmStruct;
+    public CircleStruct MusicStruct;
+    public CircleStruct EnemyStuct;
+
+    [SerializeField] 
+    private CircleEnum CircleEnum;
+
+    public CircleStruct ChosenStuct;
+
+    private void Awake()
+    {
+        switch (CircleEnum)
+        {
+            case CircleEnum.Gamer:
+                ChosenStuct = GamerStruct;
+                break;
+        }
+        Debug.Log(ChosenStuct.ToString());
+        GetComponentInChildren<SelectRandomString>().enabled = true;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
